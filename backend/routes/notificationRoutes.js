5400836cb1a8bb0ed094cd1512ad64c7cc89ffa2
kiran0038 <kiranchandra828@gmail.com>
2026-01-29 15:43:27 +0530

    Auth UI Commit

--- backend/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getMyNotifications } from "../controllers/notificationController.js";
-import { protect } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.get("/", protect, getMyNotifications);
-
-export default router;
