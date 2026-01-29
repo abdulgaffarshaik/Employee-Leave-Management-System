a68d9913c26d5d2c550c15b0ccb765d69cea74f6
unknown <harinandankota2005@gmail.com>
2026-01-29 15:30:51 +0530

    Admin UI Implemented

--- backend/config/env.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-
-const loadEnv = () => {
-  dotenv.config();
-  console.log("Environment variables loaded");
-};
-
-export default loadEnv;
--- backend/utils/passwordValidator.js
+++
@@ -1,6 +0,0 @@
-export const validatePassword = (password) => {
-  const regex =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
-
-  return regex.test(password);
-};
